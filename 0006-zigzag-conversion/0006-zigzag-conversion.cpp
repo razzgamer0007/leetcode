011b9306c683c3a1fb3ace219111ed8dba8e03bf
Mahar RazZ <mahar6775@gmail.com>
2026-08-01 21:49:41 +0530

    0006-ZIGZAG-CONVERSION Stats: Time: 0 ms (100%), Space: 14.1 MB (57.24%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+        // razzgamer0007
+    string convert(string s, int numRows) {
+        if (numRows == 1 || numRows >= s.size()) return s;
+
+        vector<string> rows(numRows);
+        int curRow = 0;
+        bool goingDown = false;
+
+        for (char c : s) {
+            rows[curRow] += c;
+            if (curRow == 0 || curRow == numRows - 1) {
+                goingDown = !goingDown;
+            }
+            curRow += goingDown ? 1 : -1;
+        }
+
+        string result;
+        for (const string& row : rows) {
+            result += row;
+        }
+        return result;
+    }
+};
--- README.md
+++ README.md
@@ -46,6 +46,7 @@ public:
 | ------- |
 | [0003-longest-substring-without-repeating-characters](https://github.com/razzgamer0007/leetcode/tree/master/0003-longest-substring-without-repeating-characters) |
 | [0005-longest-palindromic-substring](https://github.com/razzgamer0007/leetcode/tree/master/0005-longest-palindromic-substring) |
+| [0006-zigzag-conversion](https://github.com/razzgamer0007/leetcode/tree/master/0006-zigzag-conversion) |
 ## Sliding Window
 |  |
 | ------- |
