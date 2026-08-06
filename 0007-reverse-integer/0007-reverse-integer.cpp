4e89452bf521c7bed1d22d7e09c6fe12092d46d9
Mahar RazZ <mahar6775@gmail.com>
2026-08-06 20:44:41 +0530

    0007-REVERSE-INTEGER Stats: Time: 6 ms (6.43%), Space: 8.5 MB (54.96%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
+    // razzgamer0007
     int reverse(int x) {
         int ans = 0;
         while ( 0 != x) {
-            int dig = x % 10;
-            x /= 10;
             if (ans > INT_MAX / 10)
                 return 0;
             if (ans < INT_MIN / 10)
                 return 0;
-            ans = ans * 10 + dig;
+            ans = ans * 10 + (x % 10);
+            x /= 10;
         }
         return ans;
     }
